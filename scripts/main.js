03ef3fe9653cc74b9974d4a9cbb76b956d936592
Muzamil Khan <88248071+m-zamil@users.noreply.github.com>
2021-08-30 08:52:19 +0500

    home page completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,37 @@
-//Toggle favourite button
-const favouriteButtons = document.querySelectorAll(".add-to-favourite-btn");
+// let items = document.querySelectorAll(".carousel .carousel-item");
 
-for (const favBtn of favouriteButtons) {
-  favBtn.addEventListener("click", function () {
-    favBtn.classList.toggle("favourite");
+// items.forEach((el) => {
+//   const minPerSlide = 3;
+//   let next = el.nextElementSibling;
+//   for (var i = 1; i < minPerSlide; i++) {
+//     if (!next) {
+//       // wrap carousel by using first child
+
+//       next = items[0];
+//     }
+//     let cloneChild = next.cloneNode(true);
+//     el.appendChild(cloneChild.children[0]);
+//     next = next.nextElementSibling;
+//   }
+// });
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    rtl: true,
+    margin: 24,
+    loop: true,
+    autoplay: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
   });
-}
-
-function toggleFavourite() {
-  element.classList.toggle("favourite");
-}
+});
